78534188b158dc10e3f7ebc82838af7026204ae9
MihaiNuica97 <mihainuica@gmail.com>
2021-05-04 08:17:33 +0100

    limited number of synsets per word to first 5

--- hello/views.py
+++ hello/views.py
@@ -58,7 +58,7 @@ def filter_this(request):
             for word in pruned_sentence:
                 # print("Similarity for word: ", word)
                 try:
-                    synonyms = wordnet.synsets(word)
+                    synonyms = wordnet.synsets(word)[:5]
                 except:
                     continue
                 for syn in synonyms:
